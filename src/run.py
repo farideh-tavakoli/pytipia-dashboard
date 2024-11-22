389e375d3aa5c47a37d5f4212125944a6e9e54b0
Farideh Tavakoli <tavakoli.faride@gmail.com>
2024-11-22 17:59:57 +0100

    adding new features

--- src/run.py
+++ src/run.py
@@ -1,6 +1,18 @@
 import streamlit as st
+from io import StringIO
+import json
 
-st.title("Pytopia Dashboard")
-st.header("Chapter 1")
-st.text("Hi")
-st.text("hello")
+st.title(":zap: Pytopia Dashboard")
+
+uploaded_file = st.file_uploader("Choose a file")
+
+with st.expander('Statistics'):
+    if uploaded_file is not None:
+        # To convert to a string based IO:
+        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+        string_data = stringio.read()
+        st.write(string_data)
+
+
+        data = json.loads(string_data)
+        st.json(data)
