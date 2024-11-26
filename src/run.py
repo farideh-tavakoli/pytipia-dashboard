259e47780be368ef36eeeec94b5e70e3aa2013f7
Farideh Tavakoli <tavakoli.faride@gmail.com>
2024-11-26 17:12:31 +0100

    Update user landing page - Add login/signup button - Add metrics - Add statistics - Add banner - Add user info

--- src/run.py
+++ src/run.py
@@ -1,18 +1,55 @@
 import streamlit as st
-from io import StringIO
-import json
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from PIL import Image
+
+login_option = st.sidebar.radio('Login/Signup', ('Login', 'Signup'))
+
+if login_option == 'Login':
+    with st.sidebar.form("Login"):
+        st.write("Login Here")
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+
+        submitted = st.form_submit_button('Login')
+        if submitted:
+            pass
+else:
+    with st.sidebar.form("signup"):
+        st.write("Signup Here")
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+        email = st.text_input("Email")
+
+
+        submitted = st.form_submit_button('Signup')
+        if submitted:
+            pass
+
+# Banner
+banner = Image.open("./data/banner.png", )
+st.image(banner)
 
 st.title(":zap: Pytopia Dashboard")
 
-uploaded_file = st.file_uploader("Choose a file")
 
+# Metrics
+col1, col2 = st.columns(2)
+col1.metric(label="Pytopia Telegram Group", value="4000", delta="+100")
+col2.metric(label="Pytopia Website", value="2102", delta="+100")
+
+
+# Statistics
 with st.expander('Statistics'):
-    if uploaded_file is not None:
-        # To convert to a string based IO:
-        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-        string_data = stringio.read()
-        st.write(string_data)
+    fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+    sns.histplot(np.random.randn(100), ax=ax)
+    st.pyplot(fig)
 
 
-        data = json.loads(string_data)
-        st.json(data)
+# User Info
+with st.expander('User Profile:'):
+    col1, col2 = st.columns(2)
+    col1.text_input('Name:')
+    col2.text_input('Location:')
+    st.camera_input('Camera Input', key='camera_input')
